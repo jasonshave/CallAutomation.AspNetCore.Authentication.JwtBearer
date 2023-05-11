039c1beebe03c64aeef3622ce20a342a7b956937
Jason Shave <28457536+jasonshave@users.noreply.github.com>
2023-05-10 18:33:50 -0600

    add package nuspec, readme, workflows

---
+++ src/AcsJwtBearerOptions.cs
@@ -0,0 +1,8 @@
+﻿namespace CallAutomation.AspNetCore.Authentication.JwtBearer;
+
+public class AcsJwtBearerOptions
+{
+    public string? OpenIdConfigurationUrl { get; set; } = "https://acscallautomation.communication.azure.com/calling/.well-known/acsopenidconfiguration";
+
+    public string? ValidAudience { get; set; }
+}
